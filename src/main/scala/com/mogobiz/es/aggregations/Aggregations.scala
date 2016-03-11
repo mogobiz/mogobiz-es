a65965003efb89d8ae25f450b74d598ecccb7702
Stephane Manciot <stephane.manciot@ebiznext.com>
2016-03-11 16:36:40 +0100

    remove useless imports

--- src/main/scala/com/mogobiz/es/aggregations/Aggregations.scala
+++ src/main/scala/com/mogobiz/es/aggregations/Aggregations.scala
@@ -5,8 +5,6 @@
 package com.mogobiz.es.aggregations
 
 import com.sksamuel.elastic4s._
-import org.elasticsearch.search.aggregations.AggregationBuilders
-import org.elasticsearch.search.aggregations.bucket.nested.NestedBuilder
 
 object Aggregations {
 
