c48b4057a884657c39631e48a9954c870daba4f4
Hayssam Saleh <hayssam.saleh@ebiznext.com>
2016-05-30 10:25:56 +0200

    Logging

--- src/main/scala/com/mogobiz/es/Settings.scala
+++ src/main/scala/com/mogobiz/es/Settings.scala
@@ -5,10 +5,9 @@
 package com.mogobiz.es
 
 import com.typesafe.config.ConfigFactory
-import com.typesafe.scalalogging.slf4j.Logger
-import org.slf4j.LoggerFactory
+import com.typesafe.scalalogging.LazyLogging
 
-object Settings {
+object Settings extends LazyLogging {
   private val config = ConfigFactory.load("elasticsearch").withFallback(ConfigFactory.load("default-elasticsearch"))
 
   object ElasticSearch {
@@ -20,7 +19,6 @@ object Settings {
     val FullUrl = s"$Host:$HttpPort"
     val EsDebug = config.getBoolean("elasticsearch.debug")
     val bulkSize = config.getInt("elasticsearch.bulkSize")
-    private val logger = Logger(LoggerFactory.getLogger("esSettings"))
     logger.info(s"ElascticSearch on $Host:$Port, cluster->$Cluster")
   }
 
