93a39eec66cee74158e7f4c474f4be02a9950114
Hayssam Saleh <hayssam.saleh@ebiznext.com>
2018-02-05 20:58:15 +0100

    Migration to AKKA-HTTP

--- src/main/scala/com/mogobiz/es/EsClient.scala
+++ src/main/scala/com/mogobiz/es/EsClient.scala
@@ -14,10 +14,20 @@ import com.sksamuel.elastic4s.ElasticsearchClientUri
 import com.sksamuel.elastic4s.bulk.BulkCompatibleDefinition
 import com.sksamuel.elastic4s.delete.DeleteByIdDefinition
 import com.sksamuel.elastic4s.get.{GetDefinition, MultiGetDefinition}
-import com.sksamuel.elastic4s.http.ElasticDsl.{bulk => esbulk4s, delete => esdelete4s, update => esupdate4s, _}
+import com.sksamuel.elastic4s.http.ElasticDsl.{
+  bulk => esbulk4s,
+  delete => esdelete4s,
+  update => esupdate4s,
+  _
+}
 import com.sksamuel.elastic4s.http.bulk.BulkResponse
 import com.sksamuel.elastic4s.http.get.GetResponse
-import com.sksamuel.elastic4s.http.search.{SearchHit, SearchHits, SearchImplicits, SearchResponse}
+import com.sksamuel.elastic4s.http.search.{
+  SearchHit,
+  SearchHits,
+  SearchImplicits,
+  SearchResponse
+}
 import com.sksamuel.elastic4s.http.update.UpdateImplicits.UpdateHttpExecutable
 import com.sksamuel.elastic4s.http.{HttpClient, HttpExecutable}
 import com.sksamuel.elastic4s.searches.SearchDefinition
@@ -32,7 +42,7 @@ import scala.concurrent.Future
 import scala.concurrent.duration.Duration
 
 object EsClient extends SearchImplicits with LazyLogging {
-  implicit val _         = Duration.Inf
+  implicit val _ = Duration.Inf
   val client: HttpClient = HttpClient(ElasticsearchClientUri(FullUrl))
 
   def apply(): HttpClient = {
@@ -57,40 +67,54 @@ object EsClient extends SearchImplicits with LazyLogging {
       .toSet
   }
 
-  def getIndexByAlias(alias: String): List[String] = client.execute(getAlias(alias)).await.keys.toList
+  def getIndexByAlias(alias: String): List[String] =
+    client.execute(getAlias(alias)).await.keys.toList
 
-  def getUniqueIndexByAlias(alias: String): Option[String] = getIndexByAlias(alias).headOption
+  def getUniqueIndexByAlias(alias: String): Option[String] =
+    getIndexByAlias(alias).headOption
 
-  def indexLowercase[T <: AnyRef: Manifest](store: String, t: T, refresh: Boolean = false): String = {
-    val js        = JacksonConverter.serialize(t)
+  def indexLowercase[T <: AnyRef: Manifest](
+      store: String,
+      t: T,
+      refresh: Boolean = false): String = {
+    val js = JacksonConverter.serialize(t)
     val doRefresh = if (refresh) RefreshPolicy.IMMEDIATE else RefreshPolicy.NONE
     client
-      .execute(indexInto(store, manifest[T].runtimeClass.getSimpleName.toLowerCase) doc js refresh doRefresh)
+      .execute(indexInto(
+        store,
+        manifest[T].runtimeClass.getSimpleName.toLowerCase) doc js refresh doRefresh)
       .await
       .id
   }
 
-  def index[T <: Timestamped: Manifest](indexName: String, t: T, refresh: Boolean, id: Option[String] = None): String = {
+  def index[T <: Timestamped: Manifest](indexName: String,
+                                        t: T,
+                                        refresh: Boolean,
+                                        id: Option[String] = None): String = {
     val now = Calendar.getInstance().getTime
     t.dateCreated = now
     t.lastUpdated = now
-    val json      = JacksonConverter.serialize(t)
+    val json = JacksonConverter.serialize(t)
     val doRefresh = if (refresh) RefreshPolicy.IMMEDIATE else RefreshPolicy.NONE
     client
       .execute(
-          indexInto(indexName, manifest[T].runtimeClass.getSimpleName)
-            .withId(id.getOrElse(t.uuid)) doc json refresh doRefresh
+        indexInto(indexName, manifest[T].runtimeClass.getSimpleName)
+          .withId(id.getOrElse(t.uuid)) doc json refresh doRefresh
       )
       .await
       .id
   }
 
-  def simpleIndex[T <: AnyRef: Manifest](indexName: String, t: T, refresh: Boolean, id: String): String = {
-    val json      = JacksonConverter.serialize(t)
+  def simpleIndex[T <: AnyRef: Manifest](indexName: String,
+                                         t: T,
+                                         refresh: Boolean,
+                                         id: String): String = {
+    val json = JacksonConverter.serialize(t)
     val doRefresh = if (refresh) RefreshPolicy.IMMEDIATE else RefreshPolicy.NONE
     client
       .execute(
-          indexInto(indexName, manifest[T].runtimeClass.getSimpleName).withId(id) doc json refresh doRefresh
+        indexInto(indexName, manifest[T].runtimeClass.getSimpleName)
+          .withId(id) doc json refresh doRefresh
       )
       .await
       .id
@@ -109,14 +133,23 @@ object EsClient extends SearchImplicits with LazyLogging {
     load[T](indexName, uuid, manifest[T].runtimeClass.getSimpleName)
   }
 
-  def load[T: Manifest](indexName: String, uuid: String, esTypeName: String): Option[T] = {
+  def load[T: Manifest](indexName: String,
+                        uuid: String,
+                        esTypeName: String): Option[T] = {
     val res = client.execute(get(uuid) from indexName -> esTypeName).await
-    if (res.found) Some(JacksonConverter.deserialize[T](res.sourceAsString)) else None
+    if (res.found) Some(JacksonConverter.deserialize[T](res.sourceAsString))
+    else None
   }
 
-  def loadWithVersion[T: Manifest](indexName: String, uuid: String): Option[(T, Long)] = {
-    val res    = client.execute(get(uuid) from indexName -> manifest[T].runtimeClass.getSimpleName).await
-    val maybeT = if (res.found) Some(JacksonConverter.deserialize[T](res.sourceAsString)) else None
+  def loadWithVersion[T: Manifest](indexName: String,
+                                   uuid: String): Option[(T, Long)] = {
+    val res = client
+      .execute(
+        get(uuid) from indexName -> manifest[T].runtimeClass.getSimpleName)
+      .await
+    val maybeT =
+      if (res.found) Some(JacksonConverter.deserialize[T](res.sourceAsString))
+      else None
     maybeT map ((_, res.version))
   }
 
@@ -129,20 +162,37 @@ object EsClient extends SearchImplicits with LazyLogging {
     client.execute(req).await.items
   }
 
-  def delete[T: Manifest](indexName: String, uuid: String, refresh: Boolean): Boolean = {
+  def delete[T: Manifest](indexName: String,
+                          uuid: String,
+                          refresh: Boolean): Boolean = {
     delete(indexName, uuid, manifest[T].runtimeClass.getSimpleName, refresh)
   }
 
-  def delete[T: Manifest](indexName: String, uuid: String, esDocumentName: String, refresh: Boolean): Boolean = {
+  def delete[T: Manifest](indexName: String,
+                          uuid: String,
+                          esDocumentName: String,
+                          refresh: Boolean): Boolean = {
     val doRefresh = if (refresh) RefreshPolicy.IMMEDIATE else RefreshPolicy.NONE
-    val req       = esdelete4s(uuid) from indexName -> esDocumentName refresh doRefresh
-    client.execute(esdelete4s(uuid) from indexName -> esDocumentName refresh doRefresh).await.found
+    val req = esdelete4s(uuid) from indexName -> esDocumentName refresh doRefresh
+    client
+      .execute(
+        esdelete4s(uuid) from indexName -> esDocumentName refresh doRefresh)
+      .await
+      .found
   }
 
-  def bulk(requests: Seq[BulkCompatibleDefinition]): BulkResponse = client.execute(esbulk4s(requests: _*)).await
+  def bulk(requests: Seq[BulkCompatibleDefinition]): BulkResponse =
+    client.execute(esbulk4s(requests: _*)).await
 
-  def update[T <: Timestamped: Manifest](indexName: String, t: T, upsert: Boolean, refresh: Boolean): Boolean =
-    update[T](indexName, t, manifest[T].runtimeClass.getSimpleName, upsert, refresh)
+  def update[T <: Timestamped: Manifest](indexName: String,
+                                         t: T,
+                                         upsert: Boolean,
+                                         refresh: Boolean): Boolean =
+    update[T](indexName,
+              t,
+              manifest[T].runtimeClass.getSimpleName,
+              upsert,
+              refresh)
 
   def update[T <: Timestamped: Manifest](indexName: String,
                                          t: T,
@@ -151,18 +201,21 @@ object EsClient extends SearchImplicits with LazyLogging {
                                          refresh: Boolean): Boolean = {
     val now = Calendar.getInstance().getTime
     t.lastUpdated = now
-    val js        = JacksonConverter.serialize(t)
+    val js = JacksonConverter.serialize(t)
     val doRefresh = if (refresh) RefreshPolicy.IMMEDIATE else RefreshPolicy.NONE
-    val req       = esupdate4s(t.uuid) in indexName -> esDocumentName refresh doRefresh doc js docAsUpsert (upsert)
+    val req = esupdate4s(t.uuid) in indexName -> esDocumentName refresh doRefresh doc js docAsUpsert (upsert)
     client.execute(req).await.result
     true
   }
 
-  def update[T <: Timestamped: Manifest](indexName: String, t: T, version: Long): Boolean = {
+  def update[T <: Timestamped: Manifest](indexName: String,
+                                         t: T,
+                                         version: Long): Boolean = {
     val now = Calendar.getInstance().getTime
     t.lastUpdated = now
-    val js                    = JacksonConverter.serialize(t)
-    val req: UpdateDefinition = esupdate4s(t.uuid) in indexName -> manifest[T].runtimeClass.getSimpleName version version doc js
+    val js = JacksonConverter.serialize(t)
+    val req
+      : UpdateDefinition = esupdate4s(t.uuid) in indexName -> manifest[T].runtimeClass.getSimpleName version version doc js
     client.execute(req).await
     true
   }
@@ -175,15 +228,17 @@ object EsClient extends SearchImplicits with LazyLogging {
     apply().execute(req).await.result
   }
 
-  def searchAll[T: Manifest](req: SearchDefinition, fieldsDeserialize: (T, Map[String, Any]) => T = {
-    (hit: T, fields: Map[String, Any]) =>
-      hit
-  }): Seq[T] = {
+  def searchAll[T: Manifest](req: SearchDefinition,
+                             fieldsDeserialize: (T, Map[String, Any]) => T = {
+                               (hit: T, fields: Map[String, Any]) =>
+                                 hit
+                             }): Seq[T] = {
     debug(req)
     val res: SearchResponse = client.execute(req).await
     res.hits.hits.map { (hit: SearchHit) =>
       {
-        fieldsDeserialize(JacksonConverter.deserialize[T](hit.sourceAsString), hit.fields)
+        fieldsDeserialize(JacksonConverter.deserialize[T](hit.sourceAsString),
+                          hit.fields)
       }
     }
   }
@@ -249,7 +304,7 @@ object EsClient extends SearchImplicits with LazyLogging {
     */
   def searchAgg(req: SearchDefinition): JValue = {
     debug(req)
-    val res     = submit(req).await
+    val res = submit(req).await
     val resJson = parse(res.toString)
     resJson \ "aggregations"
   }
@@ -272,39 +327,41 @@ object EsClient extends SearchImplicits with LazyLogging {
     }
   }
 
-  def submit[T, U](req: T)(implicit ev1: HttpExecutable[T, U], ev2: Show[T]): Future[U] = {
+  def submit[T, U](req: T)(implicit ev1: HttpExecutable[T, U],
+                           ev2: Show[T]): Future[U] = {
     debug(req)
     apply().execute(req)
   }
 
-  import akka.stream.scaladsl._
-
-  def bulkBalancedFlow(bulkSize: Int = Settings.ElasticSearch.bulkSize,
-                       balanceSize: Int = 2): Flow[BulkCompatibleDefinition, BulkResponse] =
-    Flow() { implicit b =>
-      import FlowGraphImplicits._
-
-      val in         = UndefinedSource[BulkCompatibleDefinition]
-      val group      = Flow[BulkCompatibleDefinition].grouped(bulkSize)
-      val bulkUpsert = Flow[Seq[BulkCompatibleDefinition]].map(bulk)
-      val out        = UndefinedSink[BulkResponse]
-
-      if (balanceSize > 1) {
-
-        val balance = Balance[Seq[BulkCompatibleDefinition]]
-        val merge   = Merge[BulkResponse]
-
-        in ~> group ~> balance
-        1 to balanceSize foreach { _ =>
-          balance ~> bulkUpsert ~> merge
-        }
-        merge ~> out
-
-      } else {
-        in ~> group ~> bulkUpsert ~> out
-      }
-
-      (in, out)
-    }
+//  import akka.stream.scaladsl._
+//
+//  def bulkBalancedFlow(
+//      bulkSize: Int = Settings.ElasticSearch.bulkSize,
+//      balanceSize: Int = 2): Flow[BulkCompatibleDefinition, BulkResponse] =
+//    Flow() { implicit b =>
+//      import GraphDSL.Implicits._
+//
+//      val in = Source[BulkCompatibleDefinition]
+//      val group = Flow[BulkCompatibleDefinition].grouped(bulkSize)
+//      val bulkUpsert = Flow[Seq[BulkCompatibleDefinition]].map(bulk)
+//      val out = Sink[BulkResponse]
+//
+//      if (balanceSize > 1) {
+//
+//        val balance = Balance[Seq[BulkCompatibleDefinition]]
+//        val merge = Merge[BulkResponse]
+//
+//        in ~> group ~> balance
+//        1 to balanceSize foreach { _ =>
+//          balance ~> bulkUpsert ~> merge
+//        }
+//        merge ~> out
+//
+//      } else {
+//        in ~> group ~> bulkUpsert ~> out
+//      }
+//
+//      (in, out)
+//    }
 
 }
